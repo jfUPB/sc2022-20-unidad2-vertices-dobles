a5da3e506809ec74b80d3dbc3b129b2ccac1aea2
Cuervo01 <manuelacuervo2002@hotmail.com>
2022-09-21 15:37:01 -0500

    Create Event function

--- problem1/event.c
+++ problem1/event.c
@@ -4,10 +4,14 @@
 
 Event *CreateEvent(char *name)
 {
-
+    Event *event = malloc(sizeof(Event));
+    name[15] = 0;
+    strncpy(event->eventName, name, strlen(name)+ 1);
+    event->next = NULL;
+    return event;
 }
 
 void DestroyEvent(Event *this)
 {
-
+    free(this);
 }
--- problem1/eventList.c
+++ problem1/eventList.c
@@ -5,12 +5,16 @@
 
 EventList *CreateEventList(void)
 {
-
+    EventList *eventList = malloc(sizeof(EventList));
+    eventList->isEmpty = 1;
+    eventList->head = NULL;
+    eventList->last = NULL;
+    return eventList;
 }
 
 void DestroyEventList(EventList *this)
 {
-
+    
 }
 
 Event *SearchEvent(EventList *this, char *name)
